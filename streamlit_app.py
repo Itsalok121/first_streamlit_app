06437f155e4c12754b7d9fb42b612cd6e5a2c899
Itsalok121 <141836103+Itsalok121@users.noreply.github.com>
2023-08-09 18:06:40 +0530

    Update streamlit_app.py
    added another header

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,12 @@
 import streamlit
 
 streamlit.title('My parents new healthy diner')
+
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avancado toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
