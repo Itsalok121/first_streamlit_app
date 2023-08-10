9487a13c413b02ef1addaa7d6f2454962aead9b5
Itsalok121 <141836103+Itsalok121@users.noreply.github.com>
2023-08-10 17:22:40 +0530

    added exception handling

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit
 import pandas
 import requests
-
+import snowflake.connector
+from urllib.error import URLError
 streamlit.title('My parents new healthy diner')
 
 streamlit.header('Breakfast Menu')
@@ -26,15 +27,27 @@ streamlit.dataframe(fruits_to_show)
 #streamlit.text(fruityvice_response.json())
 streamlit.header('Fruityvice Fruit Advice!')
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+try:
+    fruit_choice = streamlit.text_input('What fruit would you like information about?')
+    if not fruit_choice:
+        streamlit.error('Please select a fruit to get information')
+    else:
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
+        fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+        streamlit.dataframe(fruityvice_normalized)
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+except URLError as e:
+    streamlit.error()
+
+
+
+
 # write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalized)
 
-import snowflake.connector
+
+streamlit.stop()
+
+
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
