031176fb535a15ed5d7dd7b1f8d2de573d9c07d2
Itsalok121 <141836103+Itsalok121@users.noreply.github.com>
2023-08-10 18:26:22 +0530

    fixed

--- streamlit_app.py
+++ streamlit_app.py
@@ -59,7 +59,7 @@ if streamlit.button('Get Fruit Load List'):
 
 def insert_new_fruit(new_fruit):
     with my_cnx.cursor() as my_cur:
-        my_cur.execute("Insert into pc_rivery_db.public.fruit_load_list values ('" + new_fruit + "')")
+        my_cur.execute("Insert into pc_rivery_db.public.fruit_load_list values ('" + str(new_fruit) + "')")
         return 'Thanks for adding ' + new_fruit
 
 if streamlit.button('Do you like to recommend more fruits?'):
